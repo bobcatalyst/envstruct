6b37f2a96c82b3cba3a2758058ea8ac84313b339
bobcatalyst <51380671+bobcatalyst@users.noreply.github.com>
2025-01-22 18:04:40 -0500

    better test

--- unmarshal_test.go
+++ unmarshal_test.go
@@ -1,15 +1,26 @@
 package envstruct
 
 import (
+    "math"
+    "math/rand"
+    "net"
     "testing"
 )
 
 func TestUnmarshal(t *testing.T) {
     type V struct {
-        Goroot string        `env:"GOROOT" description:"go root" parser:"absFile"`
-        Foo    string        `env:"FOO" description:"foo" default:"bar" parser:"string"`
-        Help   func() string `env-help:""`
+        Foo  string        `env:"FOO" description:"foo" default:"bar" parser:"string"`
+        Baz  string        `env:"BAZ" description:"baz" parser:"absFile"`
+        Host net.IP        `env:"HOST" description:"host" parser:"ipv4"`
+        Help func() string `env-help:""`
     }
+
+    d := t.TempDir()
+    t.Setenv("BAZ", d)
+
+    r8 := func() uint8 { return uint8(rand.Intn(math.MaxUint8) + 1) }
+    ip := net.IPv4(r8(), r8(), r8(), r8()).To4()
+    t.Setenv("HOST", ip.String())
 
     v, err := Unmarshal[V]()
     if err != nil {
@@ -17,11 +28,14 @@ func TestUnmarshal(t *testing.T) {
     } else if v.Help() == "" {
         t.Errorf("no help")
     } else if v.Foo != "bar" {
-        t.Errorf("foo not correct")
-    } else if len(v.Goroot) == 0 {
-        t.Errorf("goroot empty")
+        t.Errorf("foo: not set to default")
+    } else if v.Baz != d {
+        t.Errorf("baz: path not parsed correctly")
+    } else if !ip.Equal(v.Host) {
+        t.Errorf("host: not parsed correctly")
     }
-    t.Logf("GOROOT: %s", v.Goroot)
     t.Logf("FOO: %s", v.Foo)
+    t.Logf("BAZ: %s", v.Baz)
+    t.Logf("HOST: %s", v.Host)
     t.Log("\n" + v.Help())
 }
